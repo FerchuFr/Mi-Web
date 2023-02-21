49232d7b3edbb896b0850daa4c0b2beab422fbb9
FerchuFr <ferchu.fr@gmail.com>
2023-02-21 18:17:19 -0300

    Aplicamos Java

---
+++ js/app.js
@@ -0,0 +1,14 @@
+document.getElementById('button').onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById("boton").innerHTML = "Naci en Caballito, Ciudad Autónoma de Buenos Aires, Argentina"; 
+}
+
+document.getElementById('button1').onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById("boton").innerHTML = "Telecentro S.A. 2011 - Actualmente, Telefónica Argentina S.A.  2007 - 2011"; 
+}
+
+document.getElementById('button2').onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById("boton").innerHTML = "EEMN Nro. 8 Domingo Faustino Sarmiento, Morón, Escuala Juan de Garay, Rafael Castillo"; 
+}
